46ff5709263c01bb1084a462ff12af36cfe81c86
Claudio Torcato <claudiotorcato@gmail.com>
2022-03-04 08:00:30 -0300

    Atl. Nacional desclassificado

--- sorteio.py
+++ sorteio.py
@@ -96,7 +96,8 @@ grupo1 = [
     # Time("Millonarios", "millo.png", "COL", True),
     # desclassificado pelo Fluminense
     Time("Fluminense", "fluminense.png", "BRA", True),
-    Time("Atlético Nacional", "medelin.png", "COL", True),
+    # Time("Atlético Nacional", "medelin.png", "COL", True),
+    # desclassificado pelo Olimpia
     # Time("Universidade César Vallejo", "cesarvallejo.png", "PER", True),
     # desclassificado pelo Olimpia
     Time("Olímpia", "olimpia.png", "PAR", True)
