a7182298ac2f15206114a231c708596f160e9880
Claudio Torcato <claudiotorcato@gmail.com>
2022-06-01 22:19:22 -0300

    Sorteio das oitavas da Copa do Brasil 2022

--- cdb.py
+++ cdb.py
@@ -22,6 +22,13 @@ pote_b = [
 
 ]
 
+oitavas = [
+    brasil.ammg, brasil.atlpr, brasil.atlego, brasil.atletico,
+    brasil.bahia, brasil.botafogo, brasil.goias, brasil.ceara,
+    brasil.corinthians, brasil.cruzeiro, brasil.fla, brasil.fluminense,
+    brasil.fortaleza, brasil.palmeiras, brasil.santos, brasil.saopaulo
+]
+
 bp = Blueprint('cdb', __name__, url_prefix='/cdb')
 
 @bp.get('/')
@@ -38,4 +45,19 @@ def sorteio():
         partidas.append(Partida(pote_a[indice], pote_b[indice]))
     coluna1 = partidas[:8]
     coluna2 = partidas[8:]
-    return render_template('cdb/sorteio.html', coluna1=coluna1, coluna2=coluna2)
+    return render_template('cdb/sorteio.html', coluna1=coluna1, coluna2=coluna2,
+        partidas_coluna=8)
+
+@bp.get('/sorteio-oitavas')
+def sorteio_oitavas():
+    shuffle(oitavas)
+    mandantes = oitavas[:8]
+    visitantes = oitavas[8:]
+    Partida = namedtuple('Partida','mandante visitante')
+    partidas = []
+    for indice in range(8):
+        partidas.append(Partida(mandantes[indice], visitantes[indice]))
+    coluna1 = partidas[:4]
+    coluna2 = partidas[4:]
+    return render_template('cdb/sorteio.html', coluna1=coluna1, coluna2=coluna2,
+        partidas_coluna=4)
